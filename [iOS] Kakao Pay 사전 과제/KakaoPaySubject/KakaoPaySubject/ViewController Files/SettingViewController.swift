81e860259184e4ffe9048ae61f0b538b22c00894
ChangYeop-Yang <yeop9657@naver.com>
2019-01-11 13:00:57 +0900

    Create 2019.01.11. 13:00:40
    Create 2019.01.11. 13:00:40
    Kakao Pay 사전과제

---
+++ [iOS] Kakao Pay 사전 과제/KakaoPaySubject/KakaoPaySubject/ViewController Files/SettingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  KakaoPaySubject
+//
+//  Created by 양창엽 on 11/01/2019.
+//  Copyright © 2019 양창엽. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
