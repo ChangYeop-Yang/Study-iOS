fa5d8e44893571e86bde6eb969026123f7898ca6
ChangYeop-Yang <yeop9657@naver.com>
2019-01-11 13:07:27 +0900

    Update 2019.01.11. 13:06:41
    Update 2019.01.11. 13:06:41
    Cocoapod, Whipser API, README 추가

---
+++ [iOS] Kakao Pay 사전 과제/KakaoPaySubject/Pods/Target Support Files/Pods-KakaoPaySubject/Pods-KakaoPaySubject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KakaoPaySubject : NSObject
+@end
+@implementation PodsDummy_Pods_KakaoPaySubject
+@end
---
+++ [iOS] Kakao Pay 사전 과제/KakaoPaySubject/Pods/Target Support Files/Pods-KakaoPaySubjectUITests/Pods-KakaoPaySubjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KakaoPaySubjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KakaoPaySubjectUITests
+@end
