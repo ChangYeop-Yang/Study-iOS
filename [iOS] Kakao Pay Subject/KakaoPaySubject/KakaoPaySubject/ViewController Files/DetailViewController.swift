6206e44a718ea32c574ad3e0fa23b255f6fe32b4
ChangYeop-Yang <yeop9657@naver.com>
2019-01-13 21:32:53 +0900

    Update 2019.01.13. 21:32:29
    Update 2019.01.13. 21:32:29
    Detail 화면 UI/UX 설계

--- [iOS] Kakao Pay Subject/KakaoPaySubject/KakaoPaySubject/ViewController Files/DetailViewController.swift
+++ [iOS] Kakao Pay Subject/KakaoPaySubject/KakaoPaySubject/ViewController Files/DetailViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    // MARK: - Variables
+    
+    // MARK: - Outlet Variables
+    @IBOutlet private weak var titleLB: UILabel!
+    @IBOutlet private weak var tourIMG: UIImageView!
+    @IBOutlet private weak var mapIMG: UIImageView!
+    @IBOutlet private weak var webTourTB: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
